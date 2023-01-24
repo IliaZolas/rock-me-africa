553077bbeb7e304c9bec08164a5e04b2a9d8d822
Ilia Zolas <ilia@thedmcsa.co.za>
2023-01-24 16:08:40 +0100

    fix/rma - refactored app.get with try catch

--- server/index.js
+++ server/index.js
@@ -6,31 +6,51 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
-const url = 'https://www.loudersound.com/features/8-of-the-best-south-african-metal-bands-as-chosen-by-deitys-muse'
+const url = 'https://widget.rss.app/v1/wall.js'
 
 app.get('/', function (req, res) {
     res.json('webscraper running')
 })
 
-app.get('/results', (req, res) => {
-    axios(url)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const articles = []
+app.get('/results', async (req, res) => {
+    try {
+        const {data} = await axios(url)
+        const $ = cheerio.load(data)
+        const articles = []
 
-            $('h2', html).each(function () { 
-                const title = $(this).text()
-                const url = $(this).find('a').attr('href')
-                articles.push({
-                    title,
-                    url
-                })
+        $('rssapp-wall', data).each(function () { 
+            const text = $(this).text()
+            const url = $(this).find('a').attr('href')
+            articles.push({
+                text,
+                url
             })
-            res.json(articles)
-        }).catch(err => console.log(err))
-
+        })
+        res.json(articles)
+    } catch (err) {
+        console.log(err)
+    }
 })
+
+// app.get('/results', (req, res) => {
+//     axios(url)
+//         .then(response => {
+//             const html = response.data
+//             const $ = cheerio.load(html)
+//             const articles = []
+
+//             $('h2', html).each(function () { 
+//                 const title = $(this).text()
+//                 const url = $(this).find('a').attr('href')
+//                 articles.push({
+//                     title,
+//                     url
+//                 })
+//             })
+//             res.json(articles)
+//         }).catch(err => console.log(err))
+
+// })
 
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
