8afc869b157acb9fdd3a3cf9565bf8c0d4be43a3
Ilia Zolas <ilia@thedmcsa.co.za>
2023-04-17 15:22:04 +0200

    experminenting with FB Graph API

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,28 +1,28 @@
 const { default: axios } = require('axios');
-const express = require('express')
-const router = express.Router()
-require('dotenv').config()
+const express = require('express');
+const router = express.Router();
+require('dotenv').config();
 
-const usernames = ['rockmeafrica'];
-const accessToken = process.env.INSTAGRAM_BASIC_TOKEN
-const apiBaseUrl = "https://graph.instagram.com/v12.0";
+const igUserId = '1009880337';
+const accessToken = process.env.INSTAGRAM_BASIC_TOKEN;
+const apiBaseUrl = 'https://graph.instagram.com/v16.0';
 
 router.get('/instagram/posts', async (req, res) => {
   try {
     // Construct the URL for the Instagram API request
-    const url = `${apiBaseUrl}/user/media?fields=caption,media_url,permalink,thumbnail_url,username&access_token=${accessToken}&${usernames.map(u => `username=${u}`).join('&')}`;
+    const url = `${apiBaseUrl}/${igUserId}/media?fields=caption,media_url,permalink,thumbnail_url,username&access_token=${accessToken}`;
 
     // Make the request to the Instagram API
     const response = await axios.get(url);
 
     // Return the Instagram data as the response
     res.send(response.data);
-    console.log("URL:",url)
+    console.log("URL:",url);
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while retrieving Instagram data');
   }
-  console.log(accessToken)
+  console.log(accessToken);
 });
 
-module.exports = router
+module.exports = router;
